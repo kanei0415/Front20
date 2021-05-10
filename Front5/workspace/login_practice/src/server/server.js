f2ce6ccdb232ba0f99bf9d06b9a771de069e2eeb
kanei0415 <kimyoungho0415@gmail.com>
2021-05-10 21:36:19 +0900

    init

---
+++ Front5/workspace/login_practice/src/server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+const db = require("./config/db")
+
+app.get("/api/member", (req, res) => {
+  db.query("select * from member", (err, data) => {
+    if(!err) res.send({member_res : data})
+    else res.send(err);
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server On : http"//localhost:${PORT}/`);
+})
